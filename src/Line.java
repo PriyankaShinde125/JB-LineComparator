54b59cddbaf0647ec4fbf0a24587b1f07672aac8
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 15:42:49 +0530

    ADDED : Code to compare two lines using compareTo

--- src/Line.java
+++ src/Line.java
@@ -1,27 +1,30 @@
-public class Line {
+public class Line implements Comparable<Line> {
     int x1;
     int y1;
     int x2;
     int y2;
+    double length;
 
     public Line(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        this.length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     public double getLength() {
-        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         return length;
     }
 
     @Override
     public boolean equals(Object obj) {
         Line lineObj = ((Line) obj);
-        if (lineObj.x1 == this.x1 && lineObj.x2 == this.x2 && lineObj.y1 == this.y1 && lineObj.y2 == this.y2)
-            return true;
-        else
-            return false;
+        return this.length == lineObj.length;
+    }
+
+    @Override
+    public int compareTo(Line lineObj) {
+        return Double.compare(this.length, lineObj.length);
     }
 }
--- src/LineComparator.java
+++ src/LineComparator.java
@@ -7,6 +7,7 @@ public class LineComparator {
         int x2;
         int y2;
         System.out.println("Welcome to line Comparison Computation Program");
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter co-ordinate first line point 1 : ");
         x1 = sc.nextInt();
@@ -16,6 +17,7 @@ public class LineComparator {
         y2 = sc.nextInt();
         Line line1Obj = new Line(x1, y1, x2, y2);
         System.out.println("Length of Line 1 = " + line1Obj.getLength());
+
         System.out.println("Enter co-ordinate second line point 1 : ");
         x1 = sc.nextInt();
         y1 = sc.nextInt();
@@ -24,8 +26,18 @@ public class LineComparator {
         y2 = sc.nextInt();
         Line line2Obj = new Line(x1, y1, x2, y2);
         System.out.println("Length of Line 2 = " + line2Obj.getLength());
-        if (line1Obj.equals(line2Obj)){
-            System.out.println("Both Lines are equals");
+
+        int compareOutput = line1Obj.compareTo(line2Obj);
+        switch (compareOutput) {
+            case 1:
+                System.out.println("First Line is greater than second line");
+                break;
+            case -1:
+                System.out.println("First Line is less than second line");
+                break;
+            default:
+                System.out.println("Both Lines are equals");
+                break;
         }
     }
 }
