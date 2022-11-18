a23ecaa145cf7887554bfa2cf7f62d2d4e6814b3
“PriyankaShinde” <pkshinde125@gmail.com>
2022-11-18 08:15:31 +0530

    ADDED : Code to check equality of two lines

--- src/LineComparator.java
+++ src/LineComparator.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 public class LineComparator {
     public static void main(String[] args) {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
         System.out.println("Welcome to line Comparison Computation Program");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter co-ordinate for point 1 : ");
-        int line1x1 = sc.nextInt();
-        int line1y1=sc.nextInt();
-        System.out.println("Enter co-ordinate for point 2 : ");
-        int line1x2 = sc.nextInt();
-        int line1y2= sc.nextInt();
-        double line1Length=Math.sqrt(Math.pow(line1x2-line1x1,2)+Math.pow(line1y2-line1y1,2));
-        System.out.println("Length of Line 1 = "+line1Length);
+        System.out.println("Enter co-ordinate first line point 1 : ");
+        x1 = sc.nextInt();
+        y1 = sc.nextInt();
+        System.out.println("Enter co-ordinate first line point 2 : ");
+        x2 = sc.nextInt();
+        y2 = sc.nextInt();
+        Line line1Obj = new Line(x1, y1, x2, y2);
+        System.out.println("Length of Line 1 = " + line1Obj.getLength());
+        System.out.println("Enter co-ordinate second line point 1 : ");
+        x1 = sc.nextInt();
+        y1 = sc.nextInt();
+        System.out.println("Enter co-ordinate second line point 2 : ");
+        x2 = sc.nextInt();
+        y2 = sc.nextInt();
+        Line line2Obj = new Line(x1, y1, x2, y2);
+        System.out.println("Length of Line 2 = " + line2Obj.getLength());
+        if (line1Obj.equals(line2Obj)){
+            System.out.println("Both Lines are equals");
+        }
     }
 }
